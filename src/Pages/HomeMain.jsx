27170b551c49f4dfb7a874d446d983a9cf111616
Ramandeep9898 <ramank6238299@gmail.com>
2022-06-23 11:51:48 +0530

    header plus basic route

---
+++ src/Pages/HomeMain.jsx
@@ -0,0 +1,12 @@
+import react from "react";
+import React, { Component } from "react";
+import Header from "../Components/Header.Component/Header";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+export default Home;
