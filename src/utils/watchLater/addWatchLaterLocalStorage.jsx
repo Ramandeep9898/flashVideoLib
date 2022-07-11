3c7db56fcd73ddf5c5e82531541eefd328bcb0d0
Ramandeep <ramank6238299@gmail.com>
2022-07-11 15:17:32 +0530

    file sturcture

--- src/utils/watchLater/addWatchLaterLocalStorage.jsx
+++ src/utils/watchLater/addWatchLaterLocalStorage.jsx
@@ -3,8 +3,6 @@ const addWatchLaterLocalStorage = (
   INITIAL_WATCH_LATER_DATA
 ) => {
   console.log("watchLaterState1",watchLaterState)
-  // JSON.stringify(watchLaterState) !==
-  //   JSON.stringify(INITIAL_WATCH_LATER_DATA) &&
   watchLaterState && localStorage.setItem("userWatchLaterData", JSON.stringify(watchLaterState));
   
 
