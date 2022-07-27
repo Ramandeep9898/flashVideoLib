472842f0cd84d9b07aee6dfbe0c15e097cbfd872
Ramandeep <ramank6238299@gmail.com>
2022-07-27 22:19:04 +0530

    fix

--- src/Components/VideoListingPage.Component/VideoCard.Component/VideoCardMain.Component/VideoCardMain.jsx
+++ src/Components/VideoListingPage.Component/VideoCard.Component/VideoCardMain.Component/VideoCardMain.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useWatchLater } from "../../../../contexts/watchLaterContext";
+import { AiOutlineClockCircle } from "react-icons/ai";
 const VideoCardMain = ({ videos, addWatchLater }) => {
   const { watchLaterStateDispatch } = useWatchLater();
 
@@ -10,28 +11,41 @@ const VideoCardMain = ({ videos, addWatchLater }) => {
         <img
           src={videos.thumbnail_url}
           alt="image"
-          className="avatar sq-avatar card-img video-img"
+          className=" sq-avatar card-img video-img"
         />
 
         <div className="card-typo">
-          <Link to={`/videos/${videos._id}`}>
-            <div className="card capitalize fW-400 color text-left">
-              {videos.title}
+          <div className="h5 card capitalize fW-500 color text-left">
+            {videos.title}
+          </div>
+
+          <div className="card-sub card text-left mgT-8">
+            by {videos.channel_name}
+          </div>
+
+          <div className="btn-div">
+            <div className="card-sub text-left ">
+              {videos.subscribers} subscribers &#8226; {videos.veiws} veiws
             </div>
-          </Link>
-          <button
-            onClick={() => {
-              // console.log("videos",videos);
-              addWatchLater(
-                videos,
-                "ADD_TO_WATCH_LATER",
-                watchLaterStateDispatch
-              );
-            }}
-            className="btn solid-pri-btn width100 mgT-20"
-          >
-            watch later
-          </button>
+          </div>
+          <div className="btn-div">
+            <button
+              onClick={() => {
+                // console.log("videos",videos);
+                addWatchLater(
+                  videos,
+                  "ADD_TO_WATCH_LATER",
+                  watchLaterStateDispatch
+                );
+              }}
+              className="btn btn-pri-outlined  "
+            >
+              watch later
+            </button>
+            <Link to={`/videos/${videos._id}`}>
+              <button className="btn solid-pri-btn ">watch now</button>
+            </Link>
+          </div>
         </div>
       </div>
     </>
